adcb801276393c11390bf10e1594beaaba27aba2
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-24 09:50:57 +0530

    retrieved the book to be edited in page handler

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,15 @@ namespace BookListRazor.Pages.BookList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
         }
     }
 }
